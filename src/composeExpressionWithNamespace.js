aa674b75d54977c2d2f9860daac0fdf5336fbbf6
Harrison Van <harrison.van@shopback.com>
2020-05-03 12:05:59 +0700

    cleanup

--- src/composeExpressionWithNamespace.js
+++ src/composeExpressionWithNamespace.js
@@ -1,9 +1,11 @@
 /**
+ * Compose the given XPath expression with a namespace
+ *
  * @param {string} expression - The XPath expression
  * @param {string} namespace - The XML namespace
  * @returns {string}
  */
 module.exports = (expression, namespace) => {
-  const pattern = /^([./\\]*)/g;
-  return expression.replace(pattern, `$1${namespace}:`);
+  const regex = /^([./\\]*)/g;
+  return expression.replace(regex, `$1${namespace}:`);
 };
