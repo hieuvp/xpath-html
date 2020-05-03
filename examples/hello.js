63f86e0f2fd539f43c4340a9dae46de024b7069d
Harrison Van <harrison.van@shopback.com>
2020-05-03 23:21:34 +0700

    docs

--- examples/hello.js
+++ examples/hello.js
@@ -1,6 +1,10 @@
 const fs = require('fs');
 const xpath = require('xpath-html');
 
+// Assuming you are having an html file locally,
+// Don't worry much about this, you can make an HTTP request and get a response as html and
+// Send into as an input,
+// As long as, the argument is a string, everything should be fine.
 const html = fs.readFileSync(`${__dirname}/shopback.html`, 'utf8');
 const node = xpath.fromPageSource(html).findElement("//*[contains(text(), 'with love')]");
 
