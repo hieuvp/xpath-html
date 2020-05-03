047eb1c491b9ccdef45a8adf71d53e86d9e75210
Harrison Van <harrison.van@shopback.com>
2020-05-03 12:11:57 +0700

    fmt

--- src/composeExpressionWithNamespace.test.js
+++ src/composeExpressionWithNamespace.test.js
@@ -4,9 +4,12 @@ describe('composeExpressionWithNamespace', () => {
   const namespace = 'x';
 
   [
-    { expression: "//div[@class='syllabus']/*", output: "//x:div[@class='syllabus']/*" },
+    {
+      expression: "//div[@class='syllabus']/*",
+      output: "//x:div[@class='syllabus']/*",
+    },
   ].forEach(({ expression, output }) => {
-    it(`should result correctly when the expression is "${expression}"`, () =>
+    it(`should return correctly when expression is "${expression}"`, () =>
       expect(composeExpressionWithNamespace(expression, namespace)).toEqual(output));
   });
 });
