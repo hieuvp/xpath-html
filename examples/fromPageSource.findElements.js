9b6235bf06d46be729b2252c00e93393c7291497
Harrison Van <harrison.van@shopback.com>
2020-05-03 21:28:34 +0700

    docs

--- examples/fromPageSource.findElements.js
+++ examples/fromPageSource.findElements.js
@@ -2,9 +2,11 @@ const xpath = require('xpath-html');
 const fs = require('fs');
 
 const html = fs.readFileSync(`${__dirname}/shopback.html`, 'UTF-8');
-const nodes = xpath.fromPageSource(html).findElements("//*[text()='Made with love by']");
+const nodes = xpath
+  .fromPageSource(html)
+  .findElements("//img[starts-with(@src, 'https://cloud.shopback.com')]");
 
 console.log(nodes.toString());
+console.log('nodes.length =', nodes.length);
 
-console.log(`${nodes[0].localName}: ${nodes[0].firstChild.data}`);
-console.log(`Node: ${nodes[0].toString()}`);
+console.log(nodes[0].toString());
