8c1dcacb5541f103d4b33d78abbb39f670e08184
Harrison Van <harrison.van@shopback.com>
2020-05-04 10:22:22 +0700

    reset files

--- examples/fromNode.findElement.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const xpath = require('xpath-html');
-
-const html = fs.readFileSync(`${__dirname}/shopback.html`, 'utf8');
-const group = xpath.fromPageSource(html).findElement("//div[@class='ui-store-group']");
-
-const node = xpath.fromNode(group).findElement("//a[@href='/aliexpress']");
-console.log(node.toString());
