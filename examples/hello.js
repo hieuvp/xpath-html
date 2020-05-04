02ab62a2a62b0d42f6ca78f043e4172602f2869a
Harrison Van <harrison.van@shopback.com>
2020-05-04 19:18:24 +0700

    docs

--- examples/hello.js
+++ examples/hello.js
@@ -2,12 +2,12 @@ const fs = require('fs');
 const xpath = require('xpath-html');
 
 // Assuming you have an html file locally,
-// here is the content I scraped from www.shopback.sg
+// Here is the content that I scraped from www.shopback.sg
 const html = fs.readFileSync(`${__dirname}/shopback.html`, 'utf8');
 
 // Don't worry about the input much,
 // you could be able to use an HTML response of an HTTP request
-// As long as the args is a string type, everything should be fine.
+// As long as the argument is a string type, everything should be fine.
 const node = xpath.fromPageSource(html).findElement("//*[contains(text(), 'with love')]");
 
 console.log(`The matching tag is "${node.getTagName()}"`);
