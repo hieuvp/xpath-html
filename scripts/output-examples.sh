f805f70c768bb16d3c280ddd2123d55a89cfae42
Harrison Van <harrison.van@shopback.com>
2020-05-04 09:31:29 +0700

    refactoring

--- scripts/output-examples.sh
+++ scripts/output-examples.sh
@@ -2,10 +2,10 @@
 
 set -eou pipefail
 
-readonly EXAMPLE_PATH="examples"
-readonly OUTPUT_PATH="output"
+readonly EXAMPLE_DIR="examples"
+readonly OUTPUT_DIR="output"
 
-readonly REGEX="^${EXAMPLE_PATH}\/(.+)\.js$"
+readonly REGEX="^${EXAMPLE_DIR}\/(.+)\.js$"
 
 FILENAMES=$(
   git ls-files \
@@ -16,15 +16,15 @@ FILENAMES=$(
 readarray -t FILENAMES < <(printf "%s" "$FILENAMES")
 declare -ra FILENAMES
 
-rm -rf "$OUTPUT_PATH" && mkdir "$OUTPUT_PATH"
+rm -rf "$OUTPUT_DIR" && mkdir "$OUTPUT_DIR"
 
 run() {
   local -r filename=$1
-  local -r filepath="${EXAMPLE_PATH}/${filename}.js"
+  local -r filepath="${EXAMPLE_DIR}/${filename}.js"
 
-  printf "\e[33mProducing output: %s\e[0m\n" "$filepath"
+  printf "\e[33mProducing output from %s\e[0m\n" "$filepath"
 
-  node "$filepath" > "${OUTPUT_PATH}/${filename}.txt"
+  node "$filepath" > "${OUTPUT_DIR}/${filename}.txt"
 }
 
 for filename in "${FILENAMES[@]}"; do
