5b4504e25a4cc694ea417ae56535c5335317fef9
Harrison Van <harrison.van@shopback.com>
2020-05-05 19:48:58 +0700

    refactoring

--- src/extendElement.test.js
+++ src/extendElement.test.js
@@ -5,24 +5,24 @@ describe('getText', () => {
     {
       node:
         '<h6 title="Course Introduction" xmlns="http://www.w3.org/1999/xhtml">Course Introduction</h6>',
-      expectedText: 'Course Introduction',
+      output: 'Course Introduction',
     },
     {
       node:
         '<h6 title="QUIZ: Linux Daemon History" xmlns="http://www.w3.org/1999/xhtml"><!---->QUIZ: Linux Daemon History</h6>',
-      expectedText: 'QUIZ: Linux Daemon History',
+      output: 'QUIZ: Linux Daemon History',
     },
     {
       node:
         '<h6 title="Changing the Default Target of a System" xmlns="http://www.w3.org/1999/xhtml"><strong>Hands-On Lab: </strong>Changing the Default Target of a System</h6>',
-      expectedText: 'Changing the Default Target of a System',
+      output: 'Changing the Default Target of a System',
     },
   ];
 
   it('should return correctly', () => {
-    cases.forEach(({ node, expectedText }) => {
+    cases.forEach(({ node, output }) => {
       const text = xpath.fromNode(node).findElement('//h6').getText();
-      expect(text).toEqual(expectedText);
+      expect(text).toEqual(output);
     });
   });
 });
