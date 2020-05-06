6be8aececa4a4a8240525026003aa0790e3efce1
Harrison Van <harrison.van@shopback.com>
2020-05-06 10:13:50 +0700

    add some cases

--- src/composeExpressionWithNamespace.test.js
+++ src/composeExpressionWithNamespace.test.js
@@ -5,16 +5,16 @@ describe('composeExpressionWithNamespace', () => {
 
   [
     {
-      expression: "//div[@class='syllabus']/*",
-      output: "//x:div[@class='syllabus']/*",
-    },
-    {
       expression: "//div[@class='course-title']",
       output: "//x:div[@class='course-title']",
     },
     {
       expression: "//div[@class='course-title']/h1",
       output: "//x:div[@class='course-title']/x:h1",
+    },
+    {
+      expression: "//div[@class='syllabus']/*",
+      output: "//x:div[@class='syllabus']/x:*",
     },
   ].forEach(({ expression, output }) => {
     it(`should return correctly when expression is "${expression}"`, () =>
