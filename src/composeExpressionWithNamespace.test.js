6c0a774ed680170c361ff84674c1d85910f2c4bf
Harrison Van <harrison.van@shopback.com>
2020-05-06 10:06:55 +0700

    fix: compose expression

--- src/composeExpressionWithNamespace.test.js
+++ src/composeExpressionWithNamespace.test.js
@@ -8,6 +8,14 @@ describe('composeExpressionWithNamespace', () => {
       expression: "//div[@class='syllabus']/*",
       output: "//x:div[@class='syllabus']/*",
     },
+    {
+      expression: "//div[@class='course-title']",
+      output: "//x:div[@class='course-title']",
+    },
+    {
+      expression: "//div[@class='course-title']/h1",
+      output: "//x:div[@class='course-title']/x:h1",
+    },
   ].forEach(({ expression, output }) => {
     it(`should return correctly when expression is "${expression}"`, () =>
       expect(composeExpressionWithNamespace(expression, namespace)).toEqual(output));
