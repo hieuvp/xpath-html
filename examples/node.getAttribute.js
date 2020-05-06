b97dd3702f88e1bf646ef1627feee37ce7e64e1d
Harrison Van <harrison.van@shopback.com>
2020-05-06 20:14:55 +0700

    docs

--- examples/node.getAttribute.js
+++ examples/node.getAttribute.js
@@ -4,4 +4,4 @@ const xpath = require('xpath-html');
 const html = fs.readFileSync(`${__dirname}/shopback.html`, 'utf8');
 const node = xpath.fromPageSource(html).findElement("//a[text()='View All Popular Stores']");
 
-console.log('The href value is:', node.getAttribute('href'));
+console.log('The href value:', node.getAttribute('href'));
